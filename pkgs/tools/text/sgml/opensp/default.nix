8067158a611897d8a5c6e074bc68f83263e0b969
Marc Weber <marco-oweber@gmx.de>
2012-07-29 00:55:42 +0200

    opensp requires --enable-http flag to compile openjade - hard to finde

--- pkgs/tools/text/sgml/opensp/default.nix
+++ pkgs/tools/text/sgml/opensp/default.nix
@@ -10,6 +10,11 @@ stdenv.mkDerivation {
     sha256 = "1khpasr6l0a8nfz6kcf3s81vgdab8fm2dj291n5r2s53k228kx2p";
   };
 
+  # required to run docbook2html even when providing catalog !? else "URL not
+  # supported by this version" happens when running jade (test case docbook2html
+  # gutenprint.xml gutenprint cvs), --enable-http must also be passed in openjade
+  configureFlags = ["--enable-http"]; 
+
   patchPhase = ''
     sed -i s,/usr/share/sgml/docbook/xml-dtd-4.1.2/,${docbook_xml_dtd_412}/xml/dtd/docbook/, \
       docsrc/*.xml

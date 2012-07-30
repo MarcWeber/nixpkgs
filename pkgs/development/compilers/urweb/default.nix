d601a2fcc8a9b998073c3a3610d2d39d9f421ba4
Marc Weber <marco-oweber@gmx.de>
2012-07-30 15:21:22 +0200

    prepare it

--- pkgs/development/compilers/urweb/default.nix
+++ pkgs/development/compilers/urweb/default.nix
@@ -1,4 +1,6 @@
-{ stdenv, fetchurl, file, openssl, mlton, mysql, postgresql, sqlite }:
+{ stdenv, fetchurl, file, openssl, mlton, mysql, postgresql, sqlite
+, version ? "20120110"
+}:
 
 stdenv.mkDerivation rec {
   pname = "urweb";
@@ -9,6 +11,11 @@ stdenv.mkDerivation rec {
     url = "http://www.impredicative.com/ur/${name}.tgz";
     sha256 = "1f67nj2akji9dh3n2hwmcrrwd61zlrnb0xh841fpb3n20ycjzg6j";
   };
+#  # REGION AUTO UPDATE: { name="urweb"; type="hg"; url="http://hg.impredicative.com/urweb"; }
+#  src = (fetchurl { url = "http://mawercer.de/~nix/repos/urweb-hg-6e6f164.tar.bz2"; sha256 = "e725241daf3b9c31fb95b366e4371118abae326a96bdc17f32685ed38b758e39"; });
+#  name = "urweb-hg-6e6f164";
+#  # END
+
 
   buildInputs = [ stdenv.gcc file openssl mlton mysql postgresql sqlite ];
 

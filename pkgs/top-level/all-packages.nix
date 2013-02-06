f6de81c36a717a70bd21fe714f8a222c4e3664a8
Marc Weber <marco-oweber@gmx.de>
2013-02-06 08:33:16 +0100

    adding git version of imapsync

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -956,9 +956,7 @@ let
 
   imapproxy = callPackage ../tools/networking/imapproxy { };
 
-  imapsync = callPackage ../tools/networking/imapsync {
-    inherit (perlPackages) MailIMAPClient;
-  };
+  imapsync = callPackage ../tools/networking/imapsync { };
 
   inadyn = callPackage ../tools/networking/inadyn { };
 

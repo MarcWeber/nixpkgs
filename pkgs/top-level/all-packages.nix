c9b7a646071c8ad5eb573f76d335749f5a1ae989
Marc Weber <marco-oweber@gmx.de>
2013-10-26 13:35:04 +0200

    opt-out cups update:
    The new cupsd-1.6.nix file is default, you can still uncomment the old
    one.
    Both module have a new option "cupsPackages" which is the way to
    specifiy which
    cups/ghostscript versions to use.
    
    1.6.4+ comes with some changes:
    - default filter chain is PDF, gs is only used for gs printers
    - some stuff has been moved into cupsFilters and is maintained by
      openprinting now
    - cups has 2 configuration files now
    - some more less important changes such as compressing print files sent
      over
      the network and whatnot (I didn't test them)
    
    Issues:
    - printing test pages does not work yet
    
    fixes:
    - printing on canon-bjc like printers using gutentprint drivers.
      1.5.4 only prints 30% of page for whatever reason.
    
    new packages: ijs, qpdf
    ghostscript: get back the update which got reverted while keeping the
    old version

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4316,6 +4316,8 @@ let
     inherit (gnome) gtkdoc;
   };
 
+  qpdf = callPackage ../development/libraries/qpdf { };
+
   qt_gstreamer = callPackage ../development/libraries/gstreamer/qt-gstreamer {};
 
   gnet = callPackage ../development/libraries/gnet { };
@@ -4477,6 +4479,8 @@ let
   icu = callPackage ../development/libraries/icu { };
 
   id3lib = callPackage ../development/libraries/id3lib { };
+
+  ijs = callPackage ../development/libraries/ijs { };
 
   iksemel = callPackage ../development/libraries/iksemel { };
 
@@ -9788,9 +9792,23 @@ let
 
   auctex = callPackage ../tools/typesetting/tex/auctex { };
 
+  # This is for testing builds only. See cupsPackages in modules/cupsd*.nix files
   cups = callPackage ../misc/cups { libusb = libusb1; };
+  cupsPackages = thisCups: {
+    cups = thisCups;
+    cupsFilters = cupsFilters.override { cups = thisCups; };
+    gutenprint = gutenprint.override { cups = thisCups; };
+    gutenprintCVS = gutenprint.override { cups = thisCups; version = "cvs"; };
+    ghostscript = ghostscriptMainline_9_10;
+  };
+  cups16 = cupsPackages (cups.override { version = "1.6.4"; });
+  cups17 = cupsPackages (cups.override { version = "1.7.x"; });
 
+  # cups 1.5.4
   cups_pdf_filter = callPackage ../misc/cups/pdf-filter.nix { };
+
+  # cups 1.6.4+
+  cupsFilters = callPackage ../misc/cups/filters.nix { };
 
   gutenprint = callPackage ../misc/drivers/gutenprint { };
   gutenprintCVS = callPackage ../misc/drivers/gutenprint { 
@@ -9798,6 +9816,8 @@ let
     version = "cvs";
   };
 
+  # Now that even CVS gutenprint can be built I don't think we still need
+  # binaries .. (TODO remove it)
   gutenprintBin = callPackage ../misc/drivers/gutenprint/bin.nix { };
 
   cupsBjnp = callPackage ../misc/cups/drivers/cups-bjnp { };
@@ -9839,7 +9859,13 @@ let
   ghostscript = callPackage ../misc/ghostscript {
     x11Support = false;
     cupsSupport = config.ghostscript.cups or true;
-    gnuFork = config.ghostscript.gnu or false;
+    version = if config.ghostscript.gnu or false then "gnu-fork-9.04.x" else "9.06";
+  };
+
+  ghostscriptMainline_9_06 = ghostscript.override { version = "9.06"; };
+  ghostscriptMainline_9_10 = ghostscript.override { version = "9.10"; };
+  ghostscriptGnu = ghostscript.override {
+    version = "gnu-fork-9.04.x";
   };
 
   ghostscriptX = appendToName "with-X" (ghostscript.override {

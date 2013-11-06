df7da13550a8aa5ecdf733a423bd1802af5cb36a
Marc Weber <marco-oweber@gmx.de>
2013-11-07 00:14:24 +0100

    xsbt is outdated

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3757,7 +3757,6 @@ let
   scons = callPackage ../development/tools/build-managers/scons { };
 
   simpleBuildTool = callPackage ../development/tools/build-managers/simple-build-tool { };
-  xsbt = simpleBuildTool.override { version = "xsbt-scala-2.9"; };
 
   slimerjs = callPackage ../development/tools/slimerjs {};
 

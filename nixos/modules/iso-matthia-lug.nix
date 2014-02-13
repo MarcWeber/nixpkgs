50813ee9257d50dd692cdecd42ce5cf0810c499c
Your Name <you@example.com>
2014-02-13 15:35:14 +0100

    font

--- nixos/modules/iso-matthia-lug.nix
+++ nixos/modules/iso-matthia-lug.nix
@@ -136,12 +136,51 @@ in
       ./profiles/minimal.nix
     ];
 
-  services.mesa.videoDrivers = [mesa_treiber];
+  # services.mesa.videoDrivers = [mesa_treiber];
   services.xserver = {
     enable = true;
     displayManager.kdm.enable = true;
     desktopManager.kde4.enable = true;
     desktopManager.xfce.enable = true;
+  };
+
+  fonts = {
+    enableFontConfig = true;
+    enableFontDir = true;
+    extraFonts = [
+      # pkgs.fonts.droidSerif
+      # pkgs.dejavu_fonts
+      # pkgs.vistafonts
+      # pkgs.andagii
+      # pkgs.anonymousPro
+      # pkgs.arkpandora_ttf
+      # pkgs.bakoma_ttf
+      # pkgs.corefonts
+      # pkgs.clearlyU
+      # pkgs.cm_unicode
+      # pkgs.dejavu_fonts
+      # pkgs.freefont_ttf
+      # pkgs.gentium
+      # pkgs.inconsolata
+      # pkgs.junicode
+      # pkgs.liberation_ttf
+      # pkgs.libertine
+      # pkgs.lmodern
+      # pkgs.mph_2b_damase
+      # pkgs.oldstandard
+      # pkgs.theano
+      # pkgs.tempora_lgc
+      # pkgs.terminus_font
+      # pkgs.ttf_bitstream_vera
+      # pkgs.ucsFonts
+      # pkgs.unifont
+      # pkgs.vistafonts
+      # pkgs.wqy_zenhei
+      # pkgs.xfontsel
+      # pkgs.xlsfonts
+      # pkgs.base14Fonts
+      ];
+    enableGnostscriptFonts = true;
   };
 
   users.defaultUserShell = "/run/current-system/sw/bin/bash";
